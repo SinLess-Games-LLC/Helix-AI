4634011de38dd68930f21b1d55ebab60d967a485
Timothy Pierce <disdainful777@gmail.com>
2024-09-02 01:17:19 -0600

    chore: Create tests for libs

--- libs/shared/core/src/Constants/constants.ts
+++ libs/shared/core/src/Constants/constants.ts
@@ -2,16 +2,28 @@
 // Description: This file contains all the constants used in the application.
 //
 
-// Discord Status API URL
-export const DiscordStatusSummaryUrl =
-  'https://discordstatus.com/api/v2/summary.json'
-export const DiscordStatusUrl = 'https://discordstatus.com/api/v2/status.json'
-
-// Cloudflare Status API URL
-export const CloudflareStatusSummaryUrl =
-  'https://www.cloudflarestatus.com/api/v2/summary.json'
-export const CloudflareStatusUrl =
-  'https://www.cloudflarestatus.com/api/v2/status.json'
+/**
+ * @name StatusUrls
+ *
+ * @description This object contains the URLs for the status pages of Discord and Cloudflare.
+ *
+ * @property discord - The URLs for the Discord status pages.
+ * @property discord.summary - The URL for the Discord status summary.
+ * @property discord.status - The URL for the Discord status.
+ * @property cloudflare - The URLs for the Cloudflare status pages.
+ * @property cloudflare.summary - The URL for the Cloudflare status summary.
+ * @property cloudflare.status - The URL for the Cloudflare status.
+ */
+export const StatusUrls = {
+  discord: {
+    summary: 'https://discordstatus.com/api/v2/summary.json',
+    status: 'https://discordstatus.com/api/v2/status.json',
+  },
+  cloudflare: {
+    summary: 'https://www.cloudflarestatus.com/api/v2/summary.json',
+    status: 'https://www.cloudflarestatus.com/api/v2/status.json',
+  },
+}
 
 /**
  * @name HelixColors
@@ -29,10 +41,10 @@ export const CloudflareStatusUrl =
 export const HelixColors = {
   primary: {
     hex: '#F7068D',
-    rgb: 'rgb(247,6,141',
+    rgb: 'rgb(247,6,141)',
   },
   secondary: {
-    hes: '1540D1',
+    hex: '#1540D1',
     rgb: 'rgb(21,64,209)',
   },
   tertiary: {
